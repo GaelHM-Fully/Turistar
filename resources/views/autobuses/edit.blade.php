cc277a7e8ea8eaf6b0abdb7a447170dbd7655805
GaelHM-Fully <lolpee380@gmail.com>
2026-03-18 16:29:13 -0600

    cambios vistas de autobuses y controlador, se coloca default en las migraciones de autobusas

--- resources/views/autobuses/edit.blade.php
+++ resources/views/autobuses/edit.blade.php
@@ -34,14 +34,21 @@
                     <input type="number" name="capacidad_pasajeros" class="form-control" value="{{ old('capacidad_pasajeros', $autobus->capacidad_pasajeros) }}" required>
                 </div>
 
-                <div class="col-md-6">
-                    <label class="form-label">Tipo de autobús</label>
-                    <select name="tipo_autobus" class="form-select" required>
-                        <option value="Urbano" {{ old('tipo_autobus', $autobus->tipo_autobus) == 'Urbano' ? 'selected' : '' }}>Urbano</option>
-                        <option value="Interurbano" {{ old('tipo_autobus', $autobus->tipo_autobus) == 'Interurbano' ? 'selected' : '' }}>Interurbano</option>
-                        <option value="Articulado" {{ old('tipo_autobus', $autobus->tipo_autobus) == 'Articulado' ? 'selected' : '' }}>Articulado</option>
-                    </select>
-                </div>
+                @if(session('usuario_rol') === 'admin')
+                    <div class="col-md-6">
+                        <label class="form-label">Tipo de autobús</label>
+                        <select name="tipo_autobus" class="form-select" required>
+                            <option value="Urbano" {{ old('tipo_autobus', $autobus->tipo_autobus) == 'Urbano' ? 'selected' : '' }}>Urbano</option>
+                            <option value="Interurbano" {{ old('tipo_autobus', $autobus->tipo_autobus) == 'Interurbano' ? 'selected' : '' }}>Interurbano</option>
+                            <option value="Articulado" {{ old('tipo_autobus', $autobus->tipo_autobus) == 'Articulado' ? 'selected' : '' }}>Articulado</option>
+                        </select>
+                    </div>
+                @else
+                    <div class="col-md-6">
+                        <label class="form-label">Tipo de autobús</label>
+                        <input type="text" class="form-control" value="{{ $autobus->tipo_autobus }}" disabled>
+                    </div>
+                @endif
 
             </div>
 
